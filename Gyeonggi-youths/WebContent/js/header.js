922e52282d2ade11646667b2d669a41e6c50486c
So-EunPark <70000city@gmail.com>
2021-05-29 23:02:13 +0900

    front 합치기

--- Gyeonggi-youths/WebContent/js/header.js
+++ Gyeonggi-youths/WebContent/js/header.js
@@ -1,96 +1,127 @@
 window.onload = function() {
-    // 상단바
-    const toggleBtn = document.querySelector('.navbar_toggleBtn');
-    const navbarMenu = document.querySelector('.navbar_menu');
+  // 상단바
+  const toggleBtn = document.querySelector('.navbar_toggleBtn');
+  const navbarMenu = document.querySelector('.navbar_menu');
+  const navbar=document.querySelector('.navbar');
+  
+  //사용자 프로필
+  const userBtn=document.querySelector('.wrap_user');
+  const userMenu=document.querySelector('.user_menu');
+  const userArea=document.querySelector('.user_area');
+  const logoutBtn = document.querySelector('.btn_log_out');
+
+
+
+  // 메뉴 화면
+  const navFavorites = document.getElementById('nav_favorites');
+  const navSearchHistory = document.getElementById('nav_search_history');
+
+  const menuSec = document.getElementById('menu_sec');
+  const searchSec = document.getElementById('search_sec');
+
+  const favorites = document.getElementById('favorites');
+  const searchHistory = document.getElementById('search_history');
+
+  //즐겨찾기 모달 변수 
+  const favBtn = document.querySelector('.btn_store_fav');
+  const modal=document.querySelector('.modal');
+  const overlay=document.querySelector('.overlay');
+  const closeBtn=document.querySelector('.btn_close');
+  const ansNo=document.querySelector('.ans_no');
+
+  //로그아웃 모달 변수
+  const outModal=document.querySelector('.out_modal');
+
+  let menuArray = [0,0];
+  
+  toggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('active');
+    navbar.classList.toggle('active');
+    userArea.classList.toggle('active');
     
-    toggleBtn.addEventListener('click', () => {
-      navbarMenu.classList.toggle('active');
-    });
-  
-    // 메뉴 화면
-    const navFavorites = document.getElementById('nav_favorites');
-    const navSearchHistory = document.getElementById('nav_search_history');
-    const navStarReview = document.getElementById('nav_star_review');
-    const navMyPage = document.getElementById('nav_my_page');
-  
-    const menuSec = document.getElementById('menu_sec');
-    const searchSec = document.getElementById('search_sec');
-  
-    const favorites = document.getElementById('favorites');
-    const searchHistory = document.getElementById('search_history');
-    const starReview = document.getElementById('star_review');
-    let menuArray = [0,0,0];
-    
-  
-    //즐겨찾기 버튼 클릭시
-    navFavorites.addEventListener('click',()=>{
-      if(menuArray[0] == 0){
-        menuArray[0] = 1;
-        favorites.classList.add('active');
-        searchHistory.classList.remove('active');
-        starReview.classList.remove('active');
-        if(menuArray[1] == 0 && menuArray[2] == 0){
-          menuSec.classList.add('active');
-          searchSec.classList.add('menu_active');
-        }
-        if(menuArray[1] == 1 || menuArray[2] == 1){
-          menuArray[1] = 0;
-          menuArray[2] = 0;
-        }
-      }else if(menuArray[0] == 1){
-        menuArray[0] = 0;
-        favorites.classList.remove('active');
-        menuSec.classList.remove('active');
-        searchSec.classList.remove('menu_active');
-      }
-      console.log(menuArray);
-    });
-  
-    // 내 검색내역
-    navSearchHistory.addEventListener('click',()=>{
-      if(menuArray[1] == 0){
-        menuArray[1] = 1;
-        favorites.classList.remove('active');
-        searchHistory.classList.add('active');
-        starReview.classList.remove('active');
-        if(menuArray[0] == 0 && menuArray[2] == 0){
-          menuSec.classList.add('active');
-          searchSec.classList.add('menu_active');
-        }
-        if(menuArray[0] == 1 || menuArray[2] == 1){
-          menuArray[0] = 0;
-          menuArray[2] = 0;
-        }
-      }else if(menuArray[1] == 1){
+  });
+
+  userBtn.addEventListener('click',()=>{
+    userMenu.classList.toggle('active');
+  })
+
+  //즐겨찾기 버튼 클릭시
+  navFavorites.addEventListener('click',()=>{
+    //즐겨찾기 화면이 열려있지 않으면 active상태로 변경
+    if(menuArray[0] == 0){
+      if(menuArray[1] == 1){
         menuArray[1] = 0;
         searchHistory.classList.remove('active');
-        menuSec.classList.remove('active');
-        searchSec.classList.remove('menu_active');
       }
-      console.log(menuArray);
-    });
-  
-    // 내 별점 후기
-    navStarReview.addEventListener('click',()=>{
-      if(menuArray[2] == 0){
-        menuArray[2] = 1;
+      menuArray[0] = 1;
+      favorites.classList.add('active');
+      menuSec.classList.add('active');
+      searchSec.classList.add('menu_active');
+    //즐겨찾기 화면이 acive상태라면 닫기(active제거)
+    }else if(menuArray[0] == 1){
+      menuArray[0] = 0;
+      favorites.classList.remove('active');
+      menuSec.classList.remove('active');
+      searchSec.classList.remove('menu_active');
+    }
+  });
+
+  // 내 검색내역
+  navSearchHistory.addEventListener('click',()=>{
+    //내 검색내역 화면이 열려있지 않으면 active상태로 변경
+    if(menuArray[1] == 0){
+      if(menuArray[0] == 1){
+        menuArray[0] = 0;
         favorites.classList.remove('active');
-        searchHistory.classList.remove('active');
-        starReview.classList.add('active');
-        if(menuArray[0] == 0 && menuArray[1] == 0){
-          menuSec.classList.add('active');
-          searchSec.classList.add('menu_active');
-        }
-        if(menuArray[0] == 1 || menuArray[1] == 1){
-          menuArray[0] = 0;
-          menuArray[1] = 0;
-        }
-      }else if(menuArray[2] == 1){
-        menuArray[2] = 0;
-        starReview.classList.remove('active');
-        menuSec.classList.remove('active');
-        searchSec.classList.remove('menu_active');
       }
-      console.log(menuArray);
-    });
-  }
+      menuArray[1] = 1;
+      searchHistory.classList.add('active');
+      menuSec.classList.add('active');
+      searchSec.classList.add('menu_active');
+    //내 검색내역 화면이 acive상태라면 닫기(active제거)
+    }else if(menuArray[1] == 1){
+      menuArray[1] = 0;
+      searchHistory.classList.remove('active');
+      menuSec.classList.remove('active');
+      searchSec.classList.remove('menu_active');
+    }
+  });
+}
+
+  //즐겨찾기 해제 모달
+  favBtn.addEventListener('click',()=>{
+    modal.classList.add('active');
+    overlay.classList.add('active');
+  })
+  closeBtn.addEventListener('click',()=>{
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+  })
+  overlay.addEventListener('click',()=>{
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+  })
+  ansNo.addEventListener('click',()=>{
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+  })
+
+  //로그아웃 해제 모달
+  logoutBtn.addEventListener('click',()=>{
+    outModal.classList.add('active');
+    overlay.classList.add('active');
+    userMenu.classList.remove('active');
+  })
+  closeBtn.addEventListener('click',()=>{
+    outModal.classList.remove('active');
+    overlay.classList.remove('active');
+  })
+  overlay.addEventListener('click',()=>{
+    outModal.classList.remove('active');
+    overlay.classList.remove('active');
+  })
+  ansNo.addEventListener('click',()=>{
+    outModal.classList.remove('active');
+    overlay.classList.remove('active');
+  })
+
